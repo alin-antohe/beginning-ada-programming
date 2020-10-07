39ab627232003dce45bd324f015324a450052d40
Iulian Antohe <iulian.antohe@outlook.com>
2020-10-07 21:19:05 +0100

    Add examples from chapter 1

---
+++ part-1/chapter-1/examples/1/main.adb
@@ -0,0 +1,10 @@
+-- Hello, World!
+
+with Ada.Text_IO;
+
+procedure Main is
+begin
+   Ada.Text_IO.Put_Line ("Hello, World!");
+   Ada.Text_IO.Put ("It's a wonderful day!");
+   Ada.Text_IO.New_Line;
+end Main;
