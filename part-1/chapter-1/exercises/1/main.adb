a2dff12582dc7a44f31d1a8494a14a949206f847
Iulian Antohe <iulian.antohe@outlook.com>
2020-10-07 21:28:23 +0100

    Add exercises from chapter 1

---
+++ part-1/chapter-1/exercises/1/main.adb
@@ -0,0 +1,20 @@
+-- Create a small application that prints out the given output layout.
+
+with Ada.Text_IO;
+
+procedure Main is
+begin
+   Ada.Text_IO.Put_Line ("##################################################");
+   Ada.Text_IO.Put_Line ("##################################################");
+   Ada.Text_IO.Put_Line ("##                                              ##");
+   Ada.Text_IO.Put_Line ("##   00000000000   0000000000    00000000000    ##");
+   Ada.Text_IO.Put_Line ("##   00       00   00       00   00       00    ##");
+   Ada.Text_IO.Put_Line ("##   00       00   00       00   00       00    ##");
+   Ada.Text_IO.Put_Line ("##   00000000000   00       00   00000000000    ##");
+   Ada.Text_IO.Put_Line ("##   00       00   00       00   00       00    ##");
+   Ada.Text_IO.Put_Line ("##   00       00   00       00   00       00    ##");
+   Ada.Text_IO.Put_Line ("##   00       00   0000000000    00       00    ##");
+   Ada.Text_IO.Put_Line ("##                                              ##");
+   Ada.Text_IO.Put_Line ("##################################################");
+   Ada.Text_IO.Put_Line ("##################################################");
+end Main;
